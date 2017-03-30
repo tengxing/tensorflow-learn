0d925f50e18b29e008f8115a43a8a22c37e7320e
tengxing <tengxing7452@163.com>
2017-03-30 19:01:53 +0800

    tiaji

--- 预处理/学习/image_decode.py
+++ 预处理/学习/image_decode.py
@@ -12,19 +12,21 @@ with tf.Session() as sess:
     # img_data是一个张量
     img_data = tf.image.decode_jpeg(image_raw_data)
 
-    print img_data.shape
+    print img_data.eval()
+    #print sess.run(img_data)
 
     plt.imshow(img_data.eval())
     #如何直接读取图片？
     #plt.imread("test/cat.jpg")
-    plt.show()
+    #plt.show()
 
     # 还原img
     endoded_image = tf.image.encode_jpeg(img_data)
-    with tf.gfile.FastGFile("test/new_cat.jpg", 'wb') as f:
+    with tf.gfile.FastGFile("images/new_cat.jpg", 'wb') as f:
        f.write(endoded_image.eval())
 
     # convert为实数，方便读取
     img_data = tf.image.convert_image_dtype(img_data, dtype=tf.float32)
-    print img_data
     #print sess.run(img_data)
+    plt.imshow(img_data.eval())
+    plt.show()
